601b1fac9deb2682b8b79a9d1108f96ea705130c
mian <96155069+mianafzaal@users.noreply.github.com>
2022-07-10 22:25:27 +0500

    Add files via upload

--- settings.js
+++ settings.js
@@ -54,23 +54,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.vcardowner = ['923430436852'] //ur owner number
+global.ownername = "☙𝐈'𝐦 𝐌𝐈𝐀𝐍😈" //ur owner name
+global.ytname = "Mian" //ur yt chanel name
+global.socialm = "https://instagram.com/mian_savage_boy?utm_medium=copy_link" //ur github or insta name
+global.location = "Pakistan, Lahore" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
-global.botname = 'Cheems Bot MD V4' //ur bot name
-global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
+global.owner = ['923430436852']
+global.ownertag = '923430436852' //ur tag number
+global.botname = '𝐌𝐢𝐚𝐧 𝐁𝐨𝐭 𝐌𝐃 𝐕4' //ur bot name
+global.linkz = "https://instagram.com/mian_savage_boy?utm_medium=copy_link" //your theme url which will be displayed on whatsapp
+global.websitex = "https://instagram.com/mian_savage_boy?utm_medium=copy_link" //ur website to be displayed
+global.botscript = 'http://Wa.me//:+923430436852' //script link
 global.reactmoji = "😘" //ur menu react emoji
-global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
-global.author = "🦄Dream Guy Xeon" //ur sticker watermark author
+global.themeemoji = "🤓" //ur theme emoji
+global.packname = "Mian Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🤓😚♻️" //ur sticker watermark packname
+global.author = "☙𝐈'𝐦 𝐌𝐈𝐀𝐍😈" //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
@@ -79,7 +79,7 @@ global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
 global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
 
 //damtabase
-global.premium = ['916909137213'] //ur premium numbers
+global.premium = ['923430436852'] //ur premium numbers
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
@@ -105,14 +105,14 @@ global.antitags = true
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
 global.mess = {
-    success: 'Done ✓',
+    success: 'Done ✓🤓',
     admin: 'This Feature Is Only For Admin!',
     botAdmin: 'Bot Must Be Admin First!',
     owner: 'This Feature Is Only For Owner!',
     group: 'Feature Used Only For Groups!',
     private: 'Features Used Only For Private Chat!',
     bot: 'This Feature Is Only For Bot',
-    wait: 'In process...',
+    wait: 'In process🤓👀...',
     linkm: 'Where is the link?',
     error: 'Error!!',
     endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
